d85fec79da6b76cbeff64f5c0fba885f70fdb362
removedporn <86824510+removedporn@users.noreply.github.com>
2023-02-06 21:01:30 +0800

    Delete app directory

--- app/models/contact.rb
+++
@@ -1,8 +0,0 @@
-class Contact < ActiveRecord::Base
-    # Attributes arent needed here as they are assume by rails looking in db/schema.rb
-    # Contact for mvalidations
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :comments, presence: true
-    
-end
